e55c5637c3476e12f007471e2b259bd66a3b6599
Rodrigo Hernandes Alves <rodrigohernandesalves@gmail.com>
2020-05-13 10:17:35 -0300

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,23 @@
+namespace HelloWorldCSharp
+{
+    public class HelloWorld     // classe criada 
+    {
+        string _hello;
+
+        public void SetHello(string hello)
+        {
+            _hello = hello;
+        }
+
+        public string GetHello()
+        {
+             return _hello;
+        }
+    }
+}
+
+
+
+// o que e um objeto 
+//Um objeto e uma estrutura de dados, que modela as caracteriticas ou comportamentos de alguma coisa
+// no mundo real.
--- Program.cs
+++ Program.cs
@@ -1,33 +1,24 @@
 ﻿using System;
 
-namespace Array
+namespace Logica_de_programação
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-           // int n1,n2,n3,n4,n5
-           int [,] n=new int[3,5]; // sempre incia com 0 // 3 linhas e 5 colunas 
-           //int [] num=new int[3]{55,77,99};
-           //int [] num={55,77,99};
-           //int [,] num=new int [2,2]{{10, 20},{30, 40}};
+            int horario = 5;
 
-           //string[] veiculos=new string[3];
-
-           //veiculos [0] = "Carro";
-           //veiculos [1] = "Aviao";
-           //veiculos [2] = "Navio";
-
-
-
-
-           n[0,0] = 10;  n[0,1] = 20;  n[0,2] = 30; n[0,3] = 40; n[0,4] = 50;
-
-           n[1,0] = 60;  n[1,1] = 70;  n[1,2] = 80; n[1,3] = 90; n[1,4] = 15;
-
-           n[2,0] = 25;  n[2,1] = 35;  n[2,2] = 45; n[2,3] = 55; n[2,4] = 65;
-
-           Console.WriteLine(n[2,1]);
+            Console.WriteLine("Qual horario voce acordou:");
+            horario = int.Parse(Console.ReadLine());  // pode fazer com o case tambem 
+            
+            if (horario<5)
+            {
+              Console.WriteLine("Parabens voce acordou para a aula:");
+            }else
+            {   
+                Console.WriteLine("Perdeu a aula o sono ganhou:");
+                
+            }
 
         }
     }
