6e44d905e74cc946266d906652ba5e3fca9293a2
Rodrigo Hernandes Alves <rodrigohernandesalves@gmail.com>
2020-05-13 10:19:05 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,25 +1,40 @@
 ﻿using System;
 
-namespace Logica_de_programação
+namespace Vetores
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int horario = 5;
+            int[] myInts = { 5,10, 15};
+            bool[] [] myBools = new bool[2][];
 
-            Console.WriteLine("Qual horario voce acordou:");
-            horario = int.Parse(Console.ReadLine());  // pode fazer com o case tambem 
-            
-            if (horario<5)
-            {
-              Console.WriteLine("Parabens voce acordou para a aula:");
-            }else
-            {   
-                Console.WriteLine("Perdeu a aula o sono ganhou:");
-                
-            }
+            myBools[0] = new bool [2];
+            myBools[1] = new bool[1];
 
+            double[,] myDoubles = new double[2,2];
+            string[] myStrings = new string[3];
+
+            Console.WriteLine("myInts[0]:  {0}, myInts[1]: {1}, myInts[2]: {2}", myInts[0], myInts[1], myInts[2]);
+
+            myBools[0] [0] =true;
+            myBools[0] [1]=false;
+            myBools[1][0]=true;
+
+            Console.WriteLine("myBools[0] [0]:  {0}, myBools[1] [0]: {1}", myBools[0] [0], myBools[1] [0]);   
+
+            myDoubles[0, 0]=3.147;
+            myDoubles[0, 1]=7.157;
+            myDoubles[1, 1]=2.117;
+            myDoubles[1, 0]=56.00138917;
+
+            Console.WriteLine("myDoubles[0, 0]:  {0}, myDoubles[1, 0]: {1}", myDoubles[0, 0], myDoubles[1, 0]); 
+
+            myStrings[0] = "Joe";
+            myStrings[1] = "Matt";
+            myStrings[2]="Robert";
+
+            Console.WriteLine("myStrings[0]: {0},myStrings[1]: {1},myStrings[2]: {2}",myStrings[0],myStrings[1],myStrings[2]);
         }
     }
 }
